af1bfb48367f6e466d95b19aca32b80b5c4a769b
ashishjadhav58 <jadhavashish1113@gmail.com>
2025-04-07 01:54:02 +0530

    a
    a

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 const userdata = require("./Models/userdata.js");
 const message = require("./Models/message.js");
 const Drive = require("./Models/drivedetail.js")
 const Resource=require("./Models/Resouce.js")
 const TpoEvent = require("./Models/TpoEvent.js")
 const Attendence = require("./Models/Attendence.js")
-mongoose
-  .connect("mongodb+srv://evolve:evolve@evolve.o2hgi.mongodb.net/?retryWrites=true&w=majority&appName=evolve")
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("Database is connected"))
   .catch((error) => console.log("Error occurred:", error));
 
-const app = express();
-const port = 5000;
+app.use(cors({
+    origin: process.env.CLIENT_URL, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
 
 // Middlewares
 app.use(cors({
